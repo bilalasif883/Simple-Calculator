5b03d6ed3fb8d8aa8ac23e77463ca548a490f134
Muhammad Bilal <bilalasif2626@gmail.com>
2024-12-29 20:05:34 +0500

    bilal

---
+++ Main/logic.js
@@ -0,0 +1,62 @@
+let SecondNumber = '';
+let FirstNumber = '';
+let Sign = null;
+
+const display = document.getElementById('display');
+
+function ClickAdd(number) {
+    if (number === '.' && SecondNumber.includes('.'))
+        return;
+    SecondNumber = SecondNumber.toString() + number.toString();
+    updateDisplay();
+}
+
+function SelectSign(op) {
+    if (SecondNumber === '')
+        return;
+    if (FirstNumber !== '')
+        calculate();
+    Sign = op;
+    FirstNumber = SecondNumber;
+    SecondNumber = '';
+}
+
+function calculate() {
+    let answer;
+    const firstNum = parseFloat(FirstNumber);
+    const secondNum = parseFloat(SecondNumber);
+    if (isNaN(firstNum) || isNaN(secondNum)) return;
+    switch (Sign) {
+        case '+':
+            answer = firstNum + secondNum;
+            break;
+        case '-':
+            answer = firstNum - secondNum;
+            break;
+        case '*':
+            answer = firstNum * secondNum;
+            break;
+        case '/':
+            answer = secondNum === 0 ? 'Error' : firstNum / secondNum;
+            break;
+        default:
+            return;
+    }
+    SecondNumber = answer;
+    Sign = null;
+    FirstNumber = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    SecondNumber = '';
+    FirstNumber = '';
+    Sign = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerText = SecondNumber || '0';
+}
+
+clearDisplay();
